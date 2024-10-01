9c2237f9d743d701f50f3b625d486dc6a50b9ddb
Dionne Gregorio <gregoriodionne@yahoo.com>
2024-10-01 15:53:32 -0700

    revised

--- drgregor_lab1.py
+++ drgregor_lab1.py
@@ -4,27 +4,6 @@ file_name = sys.argv[1]
 file = open(file_name, 'r') 
 list = file.readlines()
 
-"""def singleton(list, start, end):
-  
-    if start == end:
-        print(list[start])
-        return None 
-   
-    mid = (start + end) // 2
-
-    if mid % 2 == 1 and list[mid] == list[mid - 1]:
-            singleton(list, mid + 1, end)
-    elif mid % 2 == 1 and list[mid] == list[mid + 1]:
-        singleton(list, start, mid - 1)
-    elif mid % 2 == 0 and list[mid] == list[mid - 1]:
-         singleton(list, start, mid-1)
-    elif mid % 2 == 0 and list[mid] == list[mid + 1]:
-         singleton(list, mid + 1, end)
-    else: 
-        print (list[mid])
-
-singleton(list, 0, len(list)-1)"""
-
 def singleton(lst, start, end):
     if start == end:
         print(lst[start])
@@ -38,3 +17,29 @@ def singleton(lst, start, end):
         singleton(lst, start, mid)
 
 singleton(list, 0, len(list)-1)
+
+
+"""def singleton(list, start, end):
+  
+    if start == end:
+        print(list[start])
+        return None 
+   
+    mid = (start + end) // 2
+
+
+        #if odd and its left is equal then call to right side
+    if mid % 2 == 1 and list[mid] == list[mid - 1]:
+            singleton(list, mid + 1, end)
+    elif mid % 2 == 1 and list[mid] == list[mid + 1]:
+        singleton(list, start, mid - 1)
+    elif mid % 2 == 0 and list[mid] == list[mid - 1]:
+         singleton(list, start, mid-1)
+    elif mid % 2 == 0 and list[mid] == list[mid + 1]:
+         singleton(list, mid + 1, end)
+    else: 
+        print (list[mid])
+
+singleton(list, 0, len(list)-1)"""
+
+
