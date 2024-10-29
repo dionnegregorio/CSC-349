6bce654e4cb0d959a3d278ab2c4a687edfd72c73
Dionne Gregorio <gregoriodionne@yahoo.com>
2024-10-28 23:24:15 -0700

    :)

--- lab2/drgregor_lab2.py
+++ lab2/drgregor_lab2.py
@@ -18,7 +18,7 @@ class node:
 
 #tarjan's algorithm
 #dfs first pass
-#keep track of the visited nodes and their 
+#keep track of the visited nodes and their lowlink value
 
 """in strong_connectivity, pass in a graph, pass that graph into dfs and
 set to variable, pass that variable to sort, return sorted graph
@@ -32,48 +32,68 @@ def strong_connectivity(G):
 	for all nodes if undiscovered, call dfs with current graph
 """
 
-def dfs(G: list[node]) -> list[node]:
+def dfs(G: list[node]) -> list[list[int]]:
 	"""
-	for each node in G, if previsit = -1
-	call explore 
-	if undiscovered, call explore on it
-
-	in explore, make its previsit = count
+	for each node in G, 
+	if previsit = -1 (undiscvered)
+	call explore  
 	"""
-	counter = 0
+	counter = [0]
+	stack = []
 	node_list = []
-	for index in range(len(G)):
-		if G[index].previsit == -1:
-			node_list = explore(G, index, counter,	node_list)
+	on_stack = [False] * len(G)
+
+	for i in range(len(G)):
+		if G[i].previsit == -1:
+			explore(G, i, counter, stack, on_stack, node_list)
 
 	return node_list
 
-			#graph		  currentnode  currcount   	node_list
-def explore(G: list[node], node: int, count: int, node_list: list[int]):
-	G[node].previsit = count
-	#using postvisit as lowlink number
-	G[node].postvisit = count
-	count = count + 1
 
-	for neighbor in G[node].out_edges:
+"""
+in explore, 
+make the current nodes pre and postvisit = to the counter
+add 1 to count 
+
+if the current nodes out edges is empty,
+add it to the list and return that list
+
+for each of the nodes neighbor, 
+if it is undiscovered, call explore on that neighbor
+if its already discovered, make the current nodes postvisit = to that neighbors post visit
+
+"""
+def explore(G: list[node], node: int, counter: list[int], stack: list[int], on_stack: list[bool], node_list: list[list[int]]):
+	G[node].previsit = G[node].postvisit = counter[0]
+	#using postvisit as lowlink number
+	counter[0] += 1
+	stack.append(node)
+	on_stack[node] = True
+
+	for neighbor in G[node].out_edges: 
 		#if neighbor is not visited, call dfs on that neighbor
+		#print(f"current node {G[node].name}, neighbor {G[neighbor].name}")
 		if G[neighbor].previsit == -1:
-			node_list = explore(G, neighbor, count,	node_list)
-			G[node].postvisit = min(G[node].in_edges, G[node].postvisit)
-		
-		elif G[neighbor].previsit > -1:
-			#if neighbor is already visited, make curren nodes postnum = to that neighbirs postnum
+			explore(G, neighbor, counter, stack, on_stack, node_list)
+			G[node].postvisit = min(G[node].postvisit, G[neighbor].postvisit)
+			#print(f"current {G[node].name} previsit and post visit: {G[node].previsit}, {G[node].postvisit}")
+
+		elif on_stack[neighbor]: 
+			#if neighbor is already visited, make current nodes postnum = to that neighbirs postnum
 			G[node].postvisit = min(G[neighbor].postvisit, G[node].postvisit)
+			#print(f"current nodes previsit and post visit: {G[node].previsit}, {G[node].postvisit}")
+
 
 	if G[node].previsit == G[node].postvisit:
 		subcomp = []
-		subcomp.append(G[node].name)
-			
+		while True:
+			n = stack.pop()
+			on_stack[n] = False
+			subcomp.append(G[n].name)
+			if n == node:
+				break
+		node_list.append(subcomp)
 
-	node_list.append(G[node].name)
-
-	return	node_list
-	
 
 def sort_component_list(components):
 	for c in components:
@@ -99,8 +119,8 @@ def main():
 	filename = sys.argv[1]
 	G = read_file(filename)
 
-	for i in range(len(G)):
-		print(f"{G[i].name}, {G[i].out_edges},  {G[i].in_edges}, {G[i].previsit}, {G[i].postvisit}, {G[i].component}")
+	#for i in range(len(G)):
+		#print(f"{G[i].name}, {G[i].out_edges},  {G[i].in_edges}, {G[i].previsit}, {G[i].postvisit}, {G[i].component}")
 	components = strong_connectivity(G)
 	print(components)
 		
